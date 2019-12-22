c69c85bacf3a36d2c6a862df8daeeec7356e14d4
gavadinov <gavadinov@gmail.com>
2019-12-22 17:32:28 +0200

    Added reader tracking.

--- db/src/storage/shard.rs
+++ db/src/storage/shard.rs
@@ -1,22 +1,27 @@
-use std::borrow::Borrow;
 use std::collections::HashMap;
-use std::mem::swap;
-use std::panic::set_hook;
-use std::sync::atomic::Ordering::{Acquire, Release};
-use std::sync::atomic::{AtomicPtr, Ordering};
+use std::sync::atomic::Ordering::{AcqRel, Acquire, Release};
+use std::sync::atomic::{AtomicBool, AtomicPtr, AtomicUsize};
 use std::sync::{Arc, Mutex, MutexGuard};
+use std::thread;
 
 type Key = Vec<u8>;
 type Val = Vec<u8>;
 type Map = HashMap<Key, Val>;
+type Counter = Arc<AtomicUsize>;
+const MIN_STALE_POINTERS: usize = 0;
 
 // TODO: Figure out how to wait for all readers to finish with the old pointer after swap. Release the mutex after that.
 // TODO: Check the smallvec crate
-// TODO: It is super optimized for reads. What about creaing a new shard and copying data from the old one. It's a huge burst of writes.
+// TODO: It is super optimized for reads. What about creating a new shard and copying data from the old one. It's a huge burst of writes.
 // TODO: Instead of storing the shard and having mut ref for the writer and immut for the readers I can have just a (reader, writer) in the shards map
 
 pub struct Reader {
     data: Arc<AtomicPtr<Map>>,
+    // false: first, true: second
+    mode: Arc<AtomicBool>,
+    // Use the Arc as a counter. After swapping the pointers wait for the ref count to go to 0
+    first: Counter,
+    second: Counter,
 }
 
 pub struct Writer {
@@ -34,17 +39,57 @@ impl Reader {
     pub fn new() -> Self {
         Self {
             data: Arc::new(AtomicPtr::new(Box::into_raw(Box::new(HashMap::new())))),
+            mode: Arc::new(AtomicBool::new(false)),
+            first: Arc::new(AtomicUsize::new(0)),
+            second: Arc::new(AtomicUsize::new(0)),
         }
     }
 
     pub fn get(&self, key: &Key) -> Option<&Val> {
-        self.reader().get(key)
+        let result;
+        let data = self.data();
+
+        self.increment_curr_counter();
+        result = data.get(key);
+        self.decrement_curr_counter();
+
+        result
     }
 
     #[inline]
-    fn reader(&self) -> &Map {
+    fn data(&self) -> &Map {
         // Unwrap should never panic because self.r is always initialized
         unsafe { self.data.load(Acquire).as_ref().unwrap() }
+    }
+
+    fn counter_count(&self, mode: bool) -> usize {
+        if mode {
+            self.second.load(Acquire)
+        } else {
+            self.first.load(Acquire)
+        }
+    }
+
+    #[inline]
+    fn decrement_curr_counter(&self) {
+        if self.mode.load(Acquire) {
+            self.second.fetch_sub(1, AcqRel);
+        } else {
+            self.first.fetch_sub(1, AcqRel);
+        }
+    }
+
+    #[inline]
+    fn increment_curr_counter(&self) {
+        if self.mode.load(Acquire) {
+            self.second.fetch_add(1, AcqRel);
+        } else {
+            self.first.fetch_add(1, AcqRel);
+        }
+    }
+
+    fn toggle_mode(&self) -> bool {
+        self.mode.fetch_xor(true, AcqRel)
     }
 }
 
@@ -52,6 +97,9 @@ impl Clone for Reader {
     fn clone(&self) -> Self {
         Self {
             data: self.data.clone(),
+            mode: self.mode.clone(),
+            first: self.first.clone(),
+            second: self.second.clone(),
         }
     }
 }
@@ -98,8 +146,25 @@ impl Writer {
         // Because Box::into_raw consumes the Box we have to keep the self.data in an Option
         // so it can be swapped with None and then put back in
         let new_data = self.reader.data.swap(Box::into_raw(data), Release);
+
+        let prev_mode = self.reader.toggle_mode();
+        self.wait(prev_mode);
+
         unsafe {
             self.data = Some(Box::from_raw(new_data));
+        }
+    }
+
+    // TODO: What if a readers is stuck holding the pointer?
+    #[inline]
+    fn wait(&mut self, prev_mode: bool) {
+        loop {
+            let stale_pointers = self.reader.counter_count(prev_mode);
+            if stale_pointers > MIN_STALE_POINTERS {
+                thread::yield_now();
+            } else {
+                break;
+            }
         }
     }
 
