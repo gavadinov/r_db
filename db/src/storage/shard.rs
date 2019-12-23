8c1bde6d9a64773dea76de8fd66a999db7208248
gavadinov <gavadinov@gmail.com>
2019-12-23 18:53:54 +0200

    Create a handle for the counter. Remove the mode and use atomic pointers.

--- db/src/storage/shard.rs
+++ db/src/storage/shard.rs
@@ -1,29 +1,35 @@
-#![allow(dead_code)]
-
 use std::collections::HashMap;
 use std::sync::atomic::Ordering::{AcqRel, Acquire, Release};
-use std::sync::atomic::{AtomicBool, AtomicPtr, AtomicUsize};
+use std::sync::atomic::{AtomicPtr, AtomicUsize};
 use std::sync::{Arc, Mutex};
-use std::thread;
 
 type Key = Vec<u8>;
 type Val = Vec<u8>;
 type Map = HashMap<Key, Val>;
-type Counter = Arc<AtomicUsize>;
-const MIN_STALE_POINTERS: usize = 0;
 
-// TODO: Figure out how to wait for all readers to finish with the old pointer after swap. Release the mutex after that.
-// TODO: Check the smallvec crate
-// TODO: It is super optimized for reads. What about creating a new shard and copying data from the old one. It's a huge burst of writes.
-// TODO: Instead of storing the shard and having mut ref for the writer and immut for the readers I can have just a (reader, writer) in the shards map
+/// A lock-free* concurrent hash map that will store the data for a single database shard.
+///
+/// Heavily optimized for reads. Reads will never block. Writes are behind a Mutex.
+/// Instead of using a reader-writer lock which will block the reads while writing the Shard keeps
+/// 2 maps behind atomic pointers. The readers read from one and the writers write to the other one.
+/// After a write the two pointers are swapped and the write is replayed to she stale map.
+///
+/// The main difficulty is keeping track of all readers that have already dereferenced a pointer to the map.
+/// To solve this every Reader increments an Atomic counter when it dereferences the pointer -> reads -> decrements the counter.
+/// The writer swaps the two pointer and then waits for the counter for the swapped map to get to 0.
+/// Then it knows that there is no one else using the map and the writes can be applied.
+///
+/// Important disadvantage is that all data is stored twice. 'Tis the cost of performance.
+
+pub struct Shard {
+    id: usize,
+    reader: Reader,
+    writer: Arc<Mutex<Writer>>,
+}
 
 pub struct Reader {
     data: Arc<AtomicPtr<Map>>,
-    // false: first, true: second
-    mode: Arc<AtomicBool>,
-    // Use the Arc as a counter. After swapping the pointers wait for the ref count to go to 0
-    first: Counter,
-    second: Counter,
+    counter: Arc<AtomicPtr<AtomicUsize>>,
 }
 
 pub struct Writer {
@@ -31,68 +37,48 @@ pub struct Writer {
     reader: Reader,
 }
 
-pub struct Shard {
-    id: u64,
-    reader: Reader,
-    writer: Arc<Mutex<Writer>>,
+struct CounterHandle<'a> {
+    counter: &'a mut AtomicUsize,
+}
+
+impl<'a> CounterHandle<'a> {
+    fn new(counter: &'a mut AtomicUsize) -> Self {
+        // Possible data race between loading the pointer and incrementing the counter :?
+        counter.fetch_add(1, AcqRel);
+        Self { counter }
+    }
+}
+
+impl<'a> Drop for CounterHandle<'a> {
+    fn drop(&mut self) {
+        self.counter.fetch_sub(1, AcqRel);
+    }
 }
 
 impl Reader {
-    pub fn new() -> Self {
+    fn new() -> Self {
         Self {
             data: Arc::new(AtomicPtr::new(Box::into_raw(Box::new(HashMap::new())))),
-            mode: Arc::new(AtomicBool::new(false)),
-            first: Arc::new(AtomicUsize::new(0)),
-            second: Arc::new(AtomicUsize::new(0)),
+            counter: Arc::new(AtomicPtr::new(Box::into_raw(Box::new(AtomicUsize::new(0))))),
+        }
+    }
+
+    fn with_data(data: Map) -> Self {
+        Self {
+            data: Arc::new(AtomicPtr::new(Box::into_raw(Box::new(data)))),
+            counter: Arc::new(AtomicPtr::new(Box::into_raw(Box::new(AtomicUsize::new(0))))),
         }
     }
 
     pub fn get(&self, key: &Key) -> Option<&Val> {
-        let result;
-        let data = self.data();
-
-        let mode = self.mode.load(Acquire);
-        self.increment_counter(mode);
-        result = data.get(key);
-        self.decrement_counter(mode);
-
-        result
+        let _handle = CounterHandle::new(unsafe { self.counter.load(Acquire).as_mut().unwrap() });
+        self.data().get(key)
     }
 
     #[inline]
     fn data(&self) -> &Map {
-        // Unwrap should never panic because self.r is always initialized
+        // Unwrap should never panic because self.r is always valid
         unsafe { self.data.load(Acquire).as_ref().unwrap() }
-    }
-
-    fn counter_count(&self, mode: bool) -> usize {
-        if mode {
-            self.second.load(Acquire)
-        } else {
-            self.first.load(Acquire)
-        }
-    }
-
-    #[inline]
-    fn decrement_counter(&self, mode: bool) {
-        if mode {
-            self.second.fetch_sub(1, AcqRel);
-        } else {
-            self.first.fetch_sub(1, AcqRel);
-        }
-    }
-
-    #[inline]
-    fn increment_counter(&self, mode: bool) {
-        if mode {
-            self.second.fetch_add(1, AcqRel);
-        } else {
-            self.first.fetch_add(1, AcqRel);
-        }
-    }
-
-    fn toggle_mode(&self) -> bool {
-        self.mode.fetch_xor(true, AcqRel)
     }
 }
 
@@ -100,18 +86,22 @@ impl Clone for Reader {
     fn clone(&self) -> Self {
         Self {
             data: self.data.clone(),
-            mode: self.mode.clone(),
-            first: self.first.clone(),
-            second: self.second.clone(),
+            counter: self.counter.clone(),
         }
     }
 }
 
 impl Writer {
-    pub fn new(r: Reader) -> Self {
+    fn new(reader: Reader) -> Self {
         Self {
             data: Some(Box::new(HashMap::new())),
-            reader: r,
+            reader,
+        }
+    }
+    fn with_data(reader: Reader, data: Map) -> Self {
+        Self {
+            data: Some(Box::new(data)),
+            reader,
         }
     }
 
@@ -122,7 +112,7 @@ impl Writer {
 
         self.swap(data);
 
-        // Writer has changed
+        // The map has changed
         let mut data = self.data();
         let result = data.insert(key, value);
         self.data = Some(data);
@@ -147,23 +137,27 @@ impl Writer {
 
     fn swap(&mut self, data: Box<Map>) {
         // Because Box::into_raw consumes the Box we have to keep the self.data in an Option
-        // so it can be swapped with None and then put back in
-        let new_data = self.reader.data.swap(Box::into_raw(data), Release);
+        // so it can be easily swapped with None and later put back in
+        let new_data = self.reader.data.swap(Box::into_raw(data), AcqRel);
 
-        let prev_mode = self.reader.toggle_mode();
-        self.wait(prev_mode);
+        let new_counter = Box::into_raw(Box::new(AtomicUsize::new(0)));
+        let counter: *mut AtomicUsize = self.reader.counter.swap(new_counter, AcqRel);
+
+        // Stick it in a Box so we don't leak memory
+        self.wait(unsafe { Box::from_raw(counter) });
 
         unsafe {
             self.data = Some(Box::from_raw(new_data));
         }
     }
 
-    // TODO: What if a readers is stuck holding the pointer?
     #[inline]
-    fn wait(&mut self, prev_mode: bool) {
+    #[allow(clippy::boxed_local)]
+    fn wait(&mut self, counter: Box<AtomicUsize>) {
+        use std::thread;
         loop {
-            let stale_pointers = self.reader.counter_count(prev_mode);
-            if stale_pointers > MIN_STALE_POINTERS {
+            let stale_pointers = counter.load(Acquire);
+            if stale_pointers > 0 {
                 thread::yield_now();
             } else {
                 break;
@@ -179,11 +173,31 @@ impl Writer {
 }
 
 impl Shard {
-    pub fn new(id: u64) -> Self {
+    pub fn new(id: usize) -> Self {
         let reader = Reader::new();
+        let writer = Writer::new(reader.clone());
+
         Self {
             id,
-            writer: Arc::new(Mutex::new(Writer::new(reader.clone()))),
+            writer: Arc::new(Mutex::new(writer)),
+            reader,
+        }
+    }
+
+    /// When autoscaling a shard will have to be moved to a different server. This constructor
+    /// will import the existing data in a more performant way.
+    pub fn with_data(id: usize, data: Map) -> Self {
+        let mut reader_data = HashMap::with_capacity(data.len());
+        data.iter().for_each(|(key, value)| {
+            reader_data.insert(key.clone(), value.clone());
+        });
+
+        let reader = Reader::with_data(reader_data);
+        let writer = Writer::with_data(reader.clone(), data);
+
+        Self {
+            id,
+            writer: Arc::new(Mutex::new(writer)),
             reader,
         }
     }
@@ -200,65 +214,96 @@ impl Shard {
 impl Drop for Shard {
     fn drop(&mut self) {
         use std::ptr;
-
-        let _handle = unsafe { Box::from_raw(self.reader.data.swap(ptr::null_mut(), Release)) };
+        // Free the raw pointers in the Reader.
+        // This is safe because by the time we drop the Shard all references to the Reader are already dropped.
+        unsafe { Box::from_raw(self.reader.data.swap(ptr::null_mut(), Release)) };
+        unsafe { Box::from_raw(self.reader.counter.swap(ptr::null_mut(), Release)) };
     }
 }
 
-#[test]
-fn test_basic() {
-    let mut s = Shard::new(42);
-    let r = s.reader();
-    assert_eq!(r.get(&vec![1 as u8]), None);
-    let w = s.writer();
-    let mut w = w.lock().unwrap();
-    w.put(vec![1], vec![2]);
-    assert_eq!(r.get(&vec![1 as u8]), Some(&vec![2 as u8]));
-    w.put(vec![1], vec![3]);
-    assert_eq!(r.get(&vec![1 as u8]), Some(&vec![3 as u8]));
-    w.delete(&vec![1 as u8]);
-    assert_eq!(r.get(&vec![1 as u8]), None);
-}
+#[cfg(test)]
+mod tests {
+    use crate::storage::Shard;
+    use std::collections::HashMap;
+    use std::thread;
 
-#[test]
-fn test_very_busy() {
-    let mut s = Shard::new(42);
-    let n = 255 as u8;
-    let readers: Vec<_> = (0..6)
-        .map(|_| {
-            let r = s.reader();
-            thread::spawn(move || {
-                let mut i = 0;
-                while i < n {
-                    match r.get(&vec![i]) {
-                        Some(val) => {
-                            assert_eq!(val, &vec![i]);
-                            i += 1;
+    #[test]
+    fn test_with_data() {
+        let mut data = HashMap::new();
+        for i in 0..10 {
+            data.insert(vec![i as u8], vec![i as u8]);
+        }
+
+        let mut s = Shard::with_data(42, data);
+        let r = s.reader();
+
+        for i in 0..10 {
+            assert_eq!(r.get(&vec![i as u8]), Some(&vec![i as u8]));
+        }
+
+        let w = s.writer();
+        let mut w = w.lock().unwrap();
+        w.put(vec![1], vec![2]);
+        assert_eq!(r.get(&vec![1 as u8]), Some(&vec![2 as u8]));
+        // Check that after the swap all the data is still there
+        assert_eq!(r.get(&vec![0 as u8]), Some(&vec![0 as u8]));
+    }
+
+    #[test]
+    fn test_basic() {
+        let mut s = Shard::new(42);
+        let r = s.reader();
+        assert_eq!(r.get(&vec![1 as u8]), None);
+        let w = s.writer();
+        let mut w = w.lock().unwrap();
+        w.put(vec![1], vec![2]);
+        assert_eq!(r.get(&vec![1 as u8]), Some(&vec![2 as u8]));
+        w.put(vec![1], vec![3]);
+        assert_eq!(r.get(&vec![1 as u8]), Some(&vec![3 as u8]));
+        w.delete(&vec![1 as u8]);
+        assert_eq!(r.get(&vec![1 as u8]), None);
+    }
+
+    #[test]
+    fn test_very_busy() {
+        let mut s = Shard::new(42);
+        let n = 255 as u8;
+        let readers: Vec<_> = (0..6)
+            .map(|_| {
+                let r = s.reader();
+                thread::spawn(move || {
+                    let mut i = 0;
+                    while i < n {
+                        match r.get(&vec![i]) {
+                            Some(val) => {
+                                assert_eq!(val, &vec![i]);
+                                i += 1;
+                            }
+                            None => thread::yield_now(),
                         }
-                        None => thread::yield_now(),
                     }
-                }
+                })
             })
-        })
-        .collect();
+            .collect();
 
-    let writers: Vec<_> = (0..4)
-        .map(|_| {
-            let lock = s.writer();
-            thread::spawn(move || {
-                for i in 0..n {
-                    let mut w = lock.lock().unwrap();
-                    w.put(vec![i], vec![i]);
-                }
+        let writers: Vec<_> = (0..4)
+            .map(|_| {
+                let lock = s.writer();
+                thread::spawn(move || {
+                    for i in 0..n {
+                        let mut w = lock.lock().unwrap();
+                        w.put(vec![i], vec![i]);
+                    }
+                })
             })
-        })
-        .collect();
+            .collect();
 
-    for handle in readers {
-        handle.join().unwrap();
-    }
+        for handle in readers {
+            handle.join().unwrap();
+        }
 
-    for handle in writers {
-        handle.join().unwrap();
+        for handle in writers {
+            handle.join().unwrap();
+        }
     }
 }
